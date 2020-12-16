2af66fcba960143b46c02078dfad331a1b572783
Li Wang <lzw158@psu.edu>
2020-12-16 02:35:22 -0500

    add_success.html does not display error

--- src/main/java/com/jiuzhang/seckill/web/SeckillActivityController.java
+++ src/main/java/com/jiuzhang/seckill/web/SeckillActivityController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.math.BigDecimal;
 import java.text.ParseException;
@@ -23,7 +22,7 @@ public class SeckillActivityController {
     @Autowired
     private SeckillActivityDao seckillActivityDao;
 
-    @ResponseBody
+    //@ResponseBody
     @RequestMapping("/addSeckillActivityAction")
     public String addSeckillActivityAction(
             @RequestParam("name") String name,
